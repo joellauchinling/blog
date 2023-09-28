2052a02ac10a54af902131be4cc3d907020efd44
Joel Lau <joel.lau.40256@gmail.com>
2023-09-29 00:31:41 +0800

    Testing on queryContent

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,11 @@
 <template>
   <main>
     <ContentDoc />
+    <!-- <NuxtLink v-for="blog in blogs" :to="blog.path">{{ blog.title }}</NuxtLink> -->
+    {{ blogs }}
   </main>
 </template>
+
+<script setup>
+const { data: blogs } = await useAsyncData('blogs', () => queryContent('blog').find())
+</script>
